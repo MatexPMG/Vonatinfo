8a1a80f7583b5f490aa9d952570aa75108329c20
MatexPMG <p.mate0629@gmail.com>
2025-11-09 16:45:49 +0100

    server redirect

--- public/main.js
+++ public/main.js
@@ -491,7 +491,7 @@ function formatTime(seconds) {
 let locoData = {};
 let locoInfoUpdater = null;
 
-fetch('https://vonatinfo-production.up.railway.app/locos.json')
+fetch('locos.json')
   .then(res => res.json())
   .then(data => {
     locoData = data;
