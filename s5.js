522453e8f05cca0a4b6e99036e86d5e87b68b75d
MatexPMG <p.mate0629@gmail.com>
2025-12-07 10:15:21 +0100

    railjet azi

--- s5.js
+++ s5.js
@@ -277,6 +277,27 @@ function secondsSinceMidnight(isoStr) {
   return (d.getHours() * 3600 + d.getMinutes() * 60 + d.getSeconds())+3600;
 }
 
+const lastPositions = {};  // { tripShortName: { lat, lon } }
+
+// heading formula
+function calculateHeading(lat1, lon1, lat2, lon2) {
+  const toRad = d => d * Math.PI / 180;
+  const toDeg = r => r * 180 / Math.PI;
+
+  const φ1 = toRad(lat1);
+  const φ2 = toRad(lat2);
+  const λ1 = toRad(lon1);
+  const λ2 = toRad(lon2);
+
+  const y = Math.sin(λ2 - λ1) * Math.cos(φ2);
+  const x =
+    Math.cos(φ1) * Math.sin(φ2) -
+    Math.sin(φ1) * Math.cos(φ2) * Math.cos(λ2 - λ1);
+
+  let brng = toDeg(Math.atan2(y, x));
+  return (brng + 360) % 360;
+}
+
 async function fetchOEBB() {
   try {
     const res = await fetch(hafas, {
@@ -304,12 +325,25 @@ for (const j of jnyL) {
 
   if (cat !== "railjet xpress") continue; // only Railjets
 
+  const tripShortName = nr + " " + cat;
+
+  let heading = null;
+  if (lastPositions[tripShortName]) {
+    const prev = lastPositions[tripShortName];
+
+    if (prev.lat !== lat || prev.lon !== lon) {
+      heading = Math.round(calculateHeading(prev.lat, prev.lon, lat, lon));
+    }
+  }
+
+  lastPositions[tripShortName] = { lat, lon };
+
   const trainObj = {
     vehicleId: "railjet",
     lat,
     lon,
-    heading: null,
-    speed: null, // ÖBB does not provide speed
+    heading,
+    speed: 1, //heading miatt
     lastUpdated: Math.floor(Date.now() / 1000),
     nextStop: { arrivalDelay: null, stopName: null },
     tripShortName: nr + " " + cat,
